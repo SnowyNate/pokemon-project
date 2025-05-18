aadef568d6ac8f0c90cf20e0f7893c6fbabb94a6
Kapooflord <omaristhenoob@gmail.com>
2025-05-18 00:25:54 -0400

    Homepage
    Added a budget nav bar still waiting on that nav bar function for typing, while also adding everything to python code while fixing some issues to make it look smoother.

--- make_pokedex.py
+++ make_pokedex.py
@@ -37,11 +37,107 @@ def dict2(twodic):
 D2 = dict2(pokemon_list)
 print(D2)
 
-with open("home.html", "w") as f:
+html_code = '''
+    <link rel="stylesheet" href="testHome.css" />
+    </head>
+    <body>    
+         <div class="background"></div>
+         <div class="content">
+         <h1>Gen 1 Pokemon</h1>
+         <h3>Nathaniel Moy, Alex Zheng, Ajmira Islam</h3>
+         <div class="intro-text">
+             <p>
+             An introduction to Pokemon, as if you the viewer didn't already know what that was.
+             </p>
+             <h3>Snorlax</h3>
+             <p>We chose Snorlax as our first partner pokemon because it is what we all aspire to be after our APs at Stuy, but are strictly prohibited from becoming.</p>
+         </div>
+         </div>
+    </body>
+    '''
+
+nav_bar = '''
+    <ul>
+        <li><a href="testHome.html">Home</a></li>
+        <li><a href="Pokedex.html">Pokedex</a></li>
+        <li><a href="top10.html">Top 10</a></li>
+    </ul>
+'''
+
+nav_bar_css = '''
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: transparent;
+}
+
+li {
+    float: right;
+}
+
+li a {
+    display: block;
+    color: black;
+    text-align: center;
+    padding: 20px 30px;
+    text-decoration: none;
+    top: 0%;
+    font-size: 20px;
+    font-family: "Montserrat";
+}
+
+li a:hover {
+    background-color: #1bb828;
+}
+'''
+
+css_code = '''
+
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
+
+.content {
+    position: relative;
+    top: 100px;
+    transform: none;
+    color: white;
+    text-align: center;
+    z-index: 1;
+    font-size: 50px;
+    font-family: "Montserrat";
+}
+
+.intro-text {
+    font-size: 0.85em;
+    color: white;
+    max-width: 600px;
+    margin: 10px auto;
+    line-height: 1.4;
+}
+
+
+.background {
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    background: url("home.jpg") no-repeat fixed center;
+    background-size: cover;
+    filter: blur(8px);
+    -webkit-filter: blur(8px);
+    z-index: 0;
+    }
+    '''
+
+with open("testHome.html", "w") as f:
     f.write('<!DOCTYPE html>\n<html lang="en">\n\t<head>\n\t\t<title>Home</title>\n')
+    f.write(nav_bar)
+    f.write(html_code)
     
-with open("home.css", "w") as f:
+with open("testHome.css", "w") as f:
     f.write('\n')
+    f.write(css_code)
+    f.write(nav_bar_css)
     
 for type in D2.keys():
     with open(f"{type}.html", "w") as f:
@@ -49,6 +145,9 @@ for type in D2.keys():
         
 with open("top10.html", "w") as f:
     f.write('<html><head><title>Top 10<title><head><html>\n')
+    
+with open("Pokedex.html", "w") as f:
+    f.write('<html><head><title>Pokedex<title><head><html>\n')
 
 
 
